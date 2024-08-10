87e9517712314ff75a91f987a294ec1fc417aac5
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-08-10 18:16:59 +0530

    added chatbot and conversation models

---
+++ backend/models/ChatBot.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./../config/db');
+
+const Chatbot = sequelize.define('Chatbot', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    slug: DataTypes.STRING,
+    url: DataTypes.STRING,
+    config: DataTypes.JSON,
+    LLMPrompts: DataTypes.JSON,
+    LLMModel: DataTypes.STRING,
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    isOnline: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    activePlan: DataTypes.STRING,
+    conversationTags: {
+      type: DataTypes.JSON,
+      defaultValue: []
+    }
+  }, {
+    tableName: 'chatbots',
+    timestamps: true
+  });
+
+  module.exports = {
+    Chatbot
+  };
---
+++ backend/models/Conversation.js
@@ -0,0 +1,75 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./../config/db');
+
+const Conversation = sequelize.define('Conversation', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    chatbotId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    agentId: {
+      type: DataTypes.UUID,
+      allowNull: true
+    },
+    assignedTo: {
+      type: DataTypes.ENUM('bot', 'agent', 'closed'),
+      allowNull: true
+    },
+    priority: {
+      type: DataTypes.ENUM('Urgent', 'Moderate', 'Low'),
+      allowNull: true
+    },
+    subject: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    source: {
+      type: DataTypes.ENUM('widget', 'whatsapp', 'tele'),
+      allowNull: true
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    subject: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    lastMsgAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    lastSeenAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    seen: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    endUserId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    conversationTags: {
+      type: DataTypes.JSON,
+      defaultValue: []
+    },
+    notes: {
+      type: DataTypes.JSON,
+      defaultValue: []
+    },
+    isHelpful: DataTypes.BOOLEAN,
+  }, {
+    tableName: 'conversations',
+    timestamps: true
+  });
+
+
+  module.exports = {
+    Conversation
+  };
